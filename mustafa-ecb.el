d8276cc4d533a9dc623f400ba7f9a3754b31cc7d
Mustafa Khattab <mustafa.khattab@gmail.com>
2011-03-03 07:57:43 -0800

    added emacs version check to mustafa-ecb

--- mustafa-ecb.el
+++ mustafa-ecb.el
@@ -2,4 +2,8 @@
 
 (add-to-list 'load-path "~/.emacs.d/vendor/ecb/")
 
-(require 'ecb)
+;; Need emacs 23.2 or greater
+(if (or (and (= emacs-major-version 23) (>= emacs-minor-version 2))
+	(> emacs-major-version 23))
+    (require 'ecb)
+  (message "You need to run Emacs version >= 23.2 for ECB"))
