5f93ccc7fceb9d826de527f33d6b3d9a51c43dbf
Mustafa Khattab <mustafa.khattab@gmail.com>
2011-03-30 16:26:28 -0700

    ECB

--- init.el
+++ init.el
@@ -12,3 +12,15 @@
 (load-library "mustafa-js-mode")
 (load-library "mustafa-ido-mode")
 (load-library "mustafa-misc-options")
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(ecb-options-version "2.40"))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
