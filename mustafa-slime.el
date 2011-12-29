32a68b7ca472dacadd97a5067bf3966d51bc0bd3
Mustafa Khattab <mustafa@mint>
2011-12-29 14:20:18 +0200

    better slime path detection, i think

--- mustafa-slime.el
+++ mustafa-slime.el
@@ -9,3 +9,15 @@
 
 (slime-setup '(slime-fancy
 	       slime-banner))
+
+(let ((slime-path (or
+		   (and (file-exists-p "/usr/local/bin/sbcl") "/usr/local/bin/sbcl")
+		   (and (file-exists-p "/usr/bin/sbcl") "/usr/bin/sbcl"))
+		  ))
+  (if slime-path
+      (progn
+	(setq inferior-lisp-program slime-path)
+	(require 'slime)
+	(slime-setup '(slime-fancy
+		       slime-banner)))
+    nil))
