ffde4b0579163819ef89159a5c9fc28162d87b03
Mustafa Khattab <mustafa.khattab@gmail.com>
2012-01-26 12:31:53 -0800

    updated gnus config

--- mustafa-gnus.el
+++ mustafa-gnus.el
@@ -5,7 +5,11 @@
 				  (nnimap-server-port 993)
 				  (nnimap-stream ssl)))
 
+(setq gnus-read-active-file 'some)
 (setq gnus-group-mode-hook 'gnus-topic-mode)
+(setq gnus-summary-thread-gathering-function 'gnus-gather-threads-by-subject)
+(setq gnus-thread-hide-subtree t)
+(setq gnus-thread-ignore-subject t)
 
 ;; Make Gnus NOT ignore [Gmail] mailboxes
 (setq gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
