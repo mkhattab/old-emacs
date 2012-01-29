8a5f6faa8df67171405a57da1f3053aec6b4099e
Mustafa Khattab <mustafa.khattab@gmail.com>
2012-01-29 15:42:59 -0800

    multi-term key bindings

--- mustafa-shell-mode.el
+++ mustafa-shell-mode.el
@@ -5,3 +5,14 @@
 ;; use multi-term
 (require 'multi-term)
 (setq multi-term-program "/bin/bash")
+
+(defun m-toggle-term-mode ()
+  (interactive)
+  (if (term-in-char-mode)
+      (term-line-mode)
+    (term-char-mode)))
+
+(add-to-list 'term-bind-key-alist '("C-c C-k" . m-toggle-term-mode))
+(add-to-list 'term-bind-key-alist '("C-c C-n" . multi-term-next))
+(add-to-list 'term-bind-key-alist '("C-c C-p" . multi-term-prev))
+(add-to-list 'term-bind-key-alist '("C-c C-t" . multi-term))
