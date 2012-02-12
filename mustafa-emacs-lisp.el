0a8efcf941e3c6ca274c01424a323516d970b9e7
Mustafa Khattab <mustafa.khattab@gmail.com>
2012-02-12 11:55:03 -0800

    disabling paredit for the time being

--- mustafa-emacs-lisp.el
+++ mustafa-emacs-lisp.el
@@ -2,7 +2,7 @@
 
 (require 'paredit)
 
-(add-hook 'emacs-lisp-mode-hook (lambda ()
-				  (m-paredit-mode-binding emacs-lisp-mode-map)))
-(add-hook 'lisp-interaction-mode-hook (lambda ()
-					(m-paredit-mode-binding lisp-interaction-mode-map)))
+;; (add-hook 'emacs-lisp-mode-hook (lambda ()
+;; 				  (m-paredit-mode-binding emacs-lisp-mode-map)))
+;; (add-hook 'lisp-interaction-mode-hook (lambda ()
+;; 					(m-paredit-mode-binding lisp-interaction-mode-map)))
