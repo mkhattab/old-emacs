4a6635873491cefc5a3cfaead7134550177d70d5
Mustafa Khattab <mustafa.khattab@gmail.com>
2012-02-12 12:10:47 -0800

    mic-paren

--- mustafa-emacs-lisp.el
+++ mustafa-emacs-lisp.el
@@ -1,8 +1,18 @@
 ;;; Emacs Lisp, including interaction mode, config
 
 (require 'paredit)
+(require 'mic-paren)
 
 ;; (add-hook 'emacs-lisp-mode-hook (lambda ()
 ;; 				  (m-paredit-mode-binding emacs-lisp-mode-map)))
 ;; (add-hook 'lisp-interaction-mode-hook (lambda ()
 ;; 					(m-paredit-mode-binding lisp-interaction-mode-map)))
+
+(paren-activate)
+(add-hook 'emacs-lisp-mode-hook (lambda ()
+				  (paren-toggle-matching-quoted-paren 1)
+				  (paren-toggle-matching-paired-delimiter 1)))
+
+(add-hook 'lisp-interaction-mode-hook (lambda ()
+				  (paren-toggle-matching-quoted-paren 1)
+				  (paren-toggle-matching-paired-delimiter 1)))
