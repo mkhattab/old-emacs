4028254f012f96f1e0c4a9988123bfc09048be85
Mustafa Khattab <mustafa.khattab@gmail.com>
2012-02-17 00:08:36 -0800

    org todo customizations

--- mustafa-org-mode.el
+++ mustafa-org-mode.el
@@ -3,3 +3,13 @@
 (setq org-hide-leading-stars t)
 (setq org-startup-folded nil)
 (setq org-time-stamp-custom-formats '("<%m/%d/%y %a>" . "<%m/%d/%y %a %I:%M %p>"))
+
+(setq org-todo-keywords '((sequence "TODO" "PENDING" "|" "DONE" "CANCELED" "DEFERRED")))
+
+(setq org-todo-keyword-faces
+      '(("PENDING" . (:foreground "gold" :weight bold))
+	("CANCELED" . (:foreground "red" :weight bold))
+	("DEFERRED" . (:foreground "purple" :weight bold))))
+
+;; note and timestamp after completing a task
+(setq org-log-done 'note)
